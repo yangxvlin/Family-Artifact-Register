f60f6057a38c024afc2e49d3689a664e659998c2
Haichao Song <haichaos@student.unimelb.edu.au>
2019-08-14 18:48:18 +1000

    sign up connect firebase

--- app/src/main/java/com/example/family_artifact_register/SignUpActivity.java
+++ app/src/main/java/com/example/family_artifact_register/SignUpActivity.java
@@ -1,17 +1,45 @@
 package com.example.family_artifact_register;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.os.Bundle;
 import android.view.View;
 import android.text.Editable;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static util.ActivityNavigator.navigateFromTo;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +63,8 @@ public class SignUpActivity extends AppCompatActivity {
 
         final TextInputLayout numberTextInput = findViewById(R.id.number_text_input);
         final TextInputEditText numberEditText = findViewById(R.id.number_edit_text);
+
+        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
         // user click "Confirm" to be directed to sign in activity
         final MaterialButton confirmButton = findViewById(R.id.confirm_button);
@@ -80,7 +110,25 @@ public class SignUpActivity extends AppCompatActivity {
                 else { numberTextInput.setError(null); }
 
                 if(allInputValid) {
-                    navigateFromTo(SignUpActivity.this, SignInActivity.class);
+                    firebaseAuth.createUserWithEmailAndPassword(emailEditText.getText().toString(), passwordEditText.getText().toString())
+                            .addOnCompleteListener(
+                                    new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                // Sign in success, update UI with the signed-in user's information
+                                                Log.d(TAG, "createUserWithEmail:success");
+                                                FirebaseUser user = firebaseAuth.getCurrentUser();
+                                                navigateFromTo(SignUpActivity.this, SignInActivity.class);
+                                            } else {
+                                                // If sign in fails, display a message to the user.
+                                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                                Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                                        Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                            );
                 }
             }
         });
@@ -98,4 +146,5 @@ public class SignUpActivity extends AppCompatActivity {
     private boolean isEmpty(@Nullable Editable text) {
         return text == null || text.length() == 0;
     }
+
 }
