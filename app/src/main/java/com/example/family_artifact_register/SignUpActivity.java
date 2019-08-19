4fd8a8205796b607009c21a83d018ba65de11a9b
Haichao Song <haichaos@student.unimelb.edu.au>
2019-08-19 14:12:05 +1000

    sign up text change

--- app/src/main/java/com/example/family_artifact_register/SignUpActivity.java
+++ app/src/main/java/com/example/family_artifact_register/SignUpActivity.java
@@ -132,7 +132,7 @@ public class SignUpActivity extends AppCompatActivity {
                                             } else {
                                                 // If sign in fails, display a message to the user.
                                                 Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                                Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                                Toast.makeText(SignUpActivity.this, "Registration failed.",
                                                         Toast.LENGTH_SHORT).show();
                                             }
                                         }
