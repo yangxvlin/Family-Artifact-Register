6aef38734df119e538d349f833d2a00fa251402b
yangxvlin <1768528843@qq.com>
2019-08-22 22:51:44 +1000

    move util package inside the code directory

--- app/src/main/java/com/example/family_artifact_register/SignUpActivity.java
+++ app/src/main/java/com/example/family_artifact_register/SignUpActivity.java
@@ -14,26 +14,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import android.os.Bundle;
-import android.view.View;
 import android.text.Editable;
-import android.util.Log;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-import static util.ActivityNavigator.navigateFromTo;
+import static com.example.family_artifact_register.util.ActivityNavigator.navigateFromTo;
 
 public class SignUpActivity extends AppCompatActivity {
 
