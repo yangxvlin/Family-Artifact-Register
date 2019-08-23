22dbc9d77feabf088a9d0f9a8fb5d98451fca7e7
Haichao Song <haichaos@student.unimelb.edu.au>
2019-08-23 16:03:43 +1000

    add phone verification to main activity

--- app/src/main/java/com/example/family_artifact_register/MainActivity.java
+++ app/src/main/java/com/example/family_artifact_register/MainActivity.java
@@ -18,22 +18,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone);
+        setContentView(R.layout.activity_main);
 
-//        // user click "Sign In" to be directed to sign in activity
-//        final Button signInButton = (Button) findViewById(R.id.sign_in_button);
-//        signInButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                navigateFromTo(MainActivity.this, SignInActivity.class);
-//            }
-//        });
-//
-//        // user click "Sign Up" to be directed to sign up activity
-//        final Button signUpButton = (Button) findViewById(R.id.sign_up_button);
-//        signUpButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                navigateFromTo(MainActivity.this, SignUpActivity.class);
-//            }
-//        });
+        // user click "Sign In" to be directed to sign in activity
+        final Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                navigateFromTo(MainActivity.this, SignInActivity.class);
+            }
+        });
+
+        // user click "Sign Up" to be directed to sign up activity
+        final Button signUpButton = (Button) findViewById(R.id.sign_up_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                navigateFromTo(MainActivity.this, SignUpActivity.class);
+            }
+        });
+
+        // user click "Phone verify" to be directed to sign up activity
+        final Button phoneVeriButton = (Button) findViewById(R.id.phone_button);
+        phoneVeriButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                navigateFromTo(MainActivity.this, PhoneVerificationActivity.class);
+            }
+        });
     }
 }
