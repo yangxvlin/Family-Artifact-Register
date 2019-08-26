a9ffb0ab86d1a21dd0a18d27436d2db443b91128
chen-dudu <liguoc@student.unimelb.edu.au>
2019-08-26 22:30:26 +1000

    clean redundant comments

--- app/src/main/java/com/example/family_artifact_register/MainActivity.java
+++ app/src/main/java/com/example/family_artifact_register/MainActivity.java
@@ -1,122 +1,39 @@
-//package com.example.family_artifact_register;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import static util.ActivityNavigator.navigateFromTo;
-//
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//
-///**
-// * @author XuLin Yang 904904,
-// * @time 2019-8-10 17:01:49
-// * @description main activity let user to choose to sign in or sign up
-// */
-//public class MainActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        // user click "Sign In" to be directed to sign in activity
-//        final Button signInButton = (Button) findViewById(R.id.sign_in_button);
-//        signInButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                navigateFromTo(MainActivity.this, SignInActivity.class);
-//            }
-//        });
-//
-//        // user click "Sign Up" to be directed to sign up activity
-//        final Button signUpButton = (Button) findViewById(R.id.sign_up_button);
-//        signUpButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                navigateFromTo(MainActivity.this, SignUpActivity.class);
-//            }
-//        });
-//    }
-//}
-
 package com.example.family_artifact_register;
 
-import android.util.Log;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import org.jetbrains.annotations.NotNull;
-import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static com.example.family_artifact_register.util.ActivityNavigator.navigateFromTo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @author XuLin Yang 904904,
- * @time 2019-8-10 17:03:06
- * @description home activity let user
+ * @time 2019-8-10 17:01:49
+ * @description main activity let user to choose to sign in or sign up
  */
 public class MainActivity extends AppCompatActivity {
-
-    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
-        // set up top app bar
-        final Toolbar toolBar = (Toolbar) findViewById(R.id.tool_bar);
-        setSupportActionBar(toolBar);
-
-        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        // both constructors work
-        // quote from the doc "please use ActionBarDrawerToggle(this, drawerLayout, open, close) (i.e. without toolbar)
-        // if you are setting the Toolbar as the Actionbar of your activity"
-//        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.drawer_open, R.string.drawer_close);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
-
-        // set up bottom navigation bar
-        BottomNavigationView bottomBar = (BottomNavigationView) findViewById(R.id.bottom_bar);
-        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                toolBar.setTitle(item.getTitle());
-                return true;
+        // user click "Sign In" to be directed to sign in activity
+        final Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                navigateFromTo(MainActivity.this, SignInActivity.class);
             }
         });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NotNull MenuItem menuItem) {
-
-        // handle navigation icon selection
-        if(drawerToggle.onOptionsItemSelected(menuItem)) {
-            return true;
-        }
-
-        // re-implement this method for more complex activity
-        // so far, this method is used only for debugging
-        switch (menuItem.getItemId()) {
-            case R.id.search:
-                Log.i("", "Enter search option.");
-                System.out.println("Enter search option.");
-                return true;
-            case R.id.more:
-                Log.i("", "Enter more option.");
-                System.out.println("Enter more option.");
-                return true;
-            default:
-                Log.i("", "Enter default case, something is wrong  !");
-                System.out.println("Enter default case, something is wrong !");
-                return super.onOptionsItemSelected(menuItem);
-        }
+        // user click "Sign Up" to be directed to sign up activity
+        final Button signUpButton = (Button) findViewById(R.id.sign_up_button);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                navigateFromTo(MainActivity.this, SignUpActivity.class);
+            }
+        });
     }
 }
