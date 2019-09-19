bdc2a33ef9af78e3a1b7144d85ca77e080192496
Dovermore <calvinyeye0627@gmail.com>
2019-09-19 12:22:23 +1000

    update auto get user location + user locatin selection

--- app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/CurrentLocationFragment.java
+++ app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/CurrentLocationFragment.java
@@ -10,28 +10,17 @@ import androidx.fragment.app.Fragment;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.media.MediaPlayer;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.family_artifact_register.MapsActivity;
 import com.example.family_artifact_register.R;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.compat.GeoDataClient;
+import com.google.android.libraries.places.compat.Place;
 import com.google.android.libraries.places.compat.PlaceDetectionClient;
 import com.google.android.libraries.places.compat.PlaceLikelihood;
 import com.google.android.libraries.places.compat.PlaceLikelihoodBufferResponse;
@@ -43,33 +32,21 @@ public class CurrentLocationFragment extends Fragment {
     private static final int PERMISSIONS_REQUEST_LOCATION = 1;
 
     // The entry points to the Places API.
-    private GeoDataClient mGeoDataClient;
     private PlaceDetectionClient mPlaceDetectionClient;
-
-    // The entry point to the Fused Location Provider.
-    private FusedLocationProviderClient mFusedLocationProviderClient;
-
-    // Stores the current user location
 
     // A default location (Sydney, Australia) and default zoom to use when location permission is
     // not granted.
-    private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
-    private static final int DEFAULT_ZOOM = 15;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean mLocationPermissionGranted = false;
 
     // Used for selecting the current place.
     private static final int M_MAX_ENTRIES = 5;
+    private Place[] mLikelyPlace = new Place[M_MAX_ENTRIES];
     private String[] mLikelyPlaceNames = new String[M_MAX_ENTRIES];
-    private String[] mLikelyPlaceAddresses = new String[M_MAX_ENTRIES];
-    private LatLng[] mLikelyPlaceLatLngs = new LatLng[M_MAX_ENTRIES];
 
-    // Store Current location of user
-    LatLng currentLatLng;
-    String currentAddress;
-    String currentPlaceName;
-
-
+    // Stores the current user location
+    private Place currentPlace;
+    private View view;
 
     public static CurrentLocationFragment newInstance() {
         return new CurrentLocationFragment();
@@ -77,42 +54,33 @@ public class CurrentLocationFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_current_location, container, false);
+        view = inflater.inflate(R.layout.fragment_current_location, container, false);
         // Find the TextView and add listener to it
-        Log.i(TAG, "Started onCreateView!");
         TextView mTextView = view.findViewById(R.id.my_location);
-        mTextView.setText("TEST");
-        Log.i(TAG, "Set Text");
+        mTextView.setText(R.string.no_location);
         mTextView.setOnClickListener(view1 -> openPlacesDialog());
-
         // Construct a PlaceDetectionClient.
         mPlaceDetectionClient = Places.getPlaceDetectionClient(getActivity());
-
-        // Construct a FusedLocationProviderClient.
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-        autoSetLocation();
-
+        // Try set location (if already have permission)
+        getLocationListAndUpdateLocation();
         return view;
     }
 
-    private void autoSetLocation() {
-        // Get current (possible) locations of user
-        getLocationList();
-        // set the location if the list is not empty
-        if (mLikelyPlaceNames[0] !=  null) {
-            currentPlaceName = mLikelyPlaceNames[0];
-            currentAddress = mLikelyPlaceAddresses[0];
-            currentLatLng = mLikelyPlaceLatLngs[0];
+    private void updateLocationField() {
+        TextView mTextView = view.findViewById(R.id.my_location);
+        if (currentPlace != null) {
+            String location = currentPlace.getName() + "\n" + currentPlace.getAddress();
+            mTextView.setText(location);
+        } else {
+            mTextView.setText(R.string.no_location);
         }
-
     }
 
     /**
      * Prompts the user to select the current place from a list of likely places, and shows the
      * current place on the map - provided the user has granted location permission.
      */
-    private void getLocationList() {
+    private void getLocationListAndUpdateLocation() {
         if (mLocationPermissionGranted) {
             // Get the likely places - that is, the businesses and other points of interest that
             // are the best match for the device's current location.
@@ -132,28 +100,28 @@ public class CurrentLocationFragment extends Fragment {
                             }
 
                             int i = 0;
-                            mLikelyPlaceNames = new String[count];
-                            mLikelyPlaceAddresses = new String[count];
-                            mLikelyPlaceLatLngs = new LatLng[count];
+
+                            mLikelyPlace = new Place[M_MAX_ENTRIES];
 
                             for (PlaceLikelihood placeLikelihood : likelyPlaces) {
-                                // Build a list of likely places to show the user.
-                                mLikelyPlaceNames[i] = (String) placeLikelihood.getPlace().getName();
-                                mLikelyPlaceAddresses[i] = (String) placeLikelihood.getPlace()
-                                        .getAddress();
-                                mLikelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
-
+                                // Build a list of likely places to show the user if user want to
+                                // open the dialog
+                                mLikelyPlace[i] = placeLikelihood.getPlace();
+                                mLikelyPlaceNames[i] = (String) mLikelyPlace[i].getName();
                                 i++;
                                 if (i > (count - 1)) {
                                     break;
                                 }
                             }
-
                             // Release the place likelihood buffer, to avoid memory leaks.
                             likelyPlaces.release();
                         } else {
                             Log.e(TAG, "Exception: %s", task.getException());
                         }
+                        if (mLikelyPlace[0] !=  null) {
+                            currentPlace = mLikelyPlace[0];
+                        }
+                        updateLocationField();
                     });
         } else {
             // The user has not granted permission.
@@ -210,7 +178,7 @@ public class CurrentLocationFragment extends Fragment {
             }
         } else {
             mLocationPermissionGranted = true;
-            getLocationList();
+            getLocationListAndUpdateLocation();
         }
     }
 
@@ -218,23 +186,26 @@ public class CurrentLocationFragment extends Fragment {
      * Displays a form allowing the user to select a place from a list of likely places.
      */
     private void openPlacesDialog() {
-        // Try get a list of locations
-        getLocationList();
-        if (mLikelyPlaceNames[0] != null) {
+        if (mLikelyPlace[0] != null) {
             // Ask the user to choose the place where they are now.
             DialogInterface.OnClickListener listener = (dialog, which) -> {
                 // The "which" argument contains the position of the selected item.
-                // TODO adapt this to change the class attribute instead.
-                currentPlaceName = mLikelyPlaceNames[which];
-                currentLatLng = mLikelyPlaceLatLngs[which];
-                currentAddress = mLikelyPlaceAddresses[which];
+                currentPlace = mLikelyPlace[which];
+                updateLocationField();
             };
+
             // Display the dialog.
             AlertDialog dialog = new AlertDialog.Builder(getActivity())
                     .setTitle(R.string.pick_place)
                     .setItems(mLikelyPlaceNames, listener)
                     .show();
+        } else {
+            getLocationListAndUpdateLocation();
+            Toast.makeText(getActivity(), "Fetching Locations, Try again.", Toast.LENGTH_SHORT).show();
         }
     }
 
+    public Place getPlace() {
+        return currentPlace;
+    }
 }
