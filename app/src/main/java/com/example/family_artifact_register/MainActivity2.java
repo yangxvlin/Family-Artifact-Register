11f4d70ef2b4c77a1537447456e9787516524cbe
yangxvlin <1768528843@qq.com>
2019-09-20 15:03:21 +1000

    add new code

---
+++ app/src/main/java/com/example/family_artifact_register/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.family_artifact_register;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
