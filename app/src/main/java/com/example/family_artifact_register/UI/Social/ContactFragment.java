aabf3d3ac074b53a9eea462beb6f26007ef8c414
chen-dudu <liguoc@student.unimelb.edu.au>
2019-09-21 18:20:01 +1000

    displayed friend list in fragment

--- app/src/main/java/com/example/family_artifact_register/UI/Social/ContactFragment.java
+++ app/src/main/java/com/example/family_artifact_register/UI/Social/ContactFragment.java
@@ -6,10 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.family_artifact_register.R;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ContactFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+    private DividerItemDecoration divider;
+
     public ContactFragment() {
         // Required empty public constructor
     }
@@ -17,9 +29,37 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        ArrayList<String> dataSet = new ArrayList<>();
+
+        // fake data for testing use only
+        String[] friends = new String[] {"Tim", "Matt", "Leon", "coffee", "xulin", "zhuoqun", "haichao", "1", "2", "3", "4"};
+        Collections.addAll(dataSet, friends);
+
+        setupRecyclerView(view, dataSet);
+
+        return view;
     }
 
     public static ContactFragment newInstance() { return new ContactFragment(); }
+
+    private void setupRecyclerView(View view, ArrayList<String> dataSet) {
+        // get the view
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        // set layout manager for the view
+        layoutManager = new LinearLayoutManager(view.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // set the adapter for the view
+        adapter = new FriendListAdapter(dataSet);
+        recyclerView.setAdapter(adapter);
+
+        // set the divider between list item
+        divider = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.addItemDecoration(divider);
+    }
 }
 
