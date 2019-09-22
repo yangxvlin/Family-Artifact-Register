66c48e970a1da5fb5b6c2ed1c93222dc44486d09
Dovermore <calvinyeye0627@gmail.com>
2019-09-22 18:37:58 +1000

    update strategy pattern for camera positions

--- app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/MultipleMarkerZoomStrategy.java
+++ app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/MultipleMarkerZoomStrategy.java
@@ -1,6 +1,9 @@
 package com.example.family_artifact_register.UI.MapServiceFragment;
 
+
 import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 
 import java.util.List;
@@ -8,6 +11,12 @@ import java.util.List;
 public class MultipleMarkerZoomStrategy extends BaseMakerZoomStrategy {
     @Override
     CameraUpdate makeCameraUpdate(List<Marker> markers) {
-        return null;// TODO (Dovermore, 2019-09-22): Implement makeCameraUpdate
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (Marker marker : markers) {
+            builder.include(marker.getPosition());
+        }
+        int padding = 100; // offset from edges of the map in pixels
+        LatLngBounds bounds = builder.build();
+        return CameraUpdateFactory.newLatLngBounds(bounds, padding);
     }
 }
--- app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/SingleMarkerZoomStrategy.java
+++ app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/SingleMarkerZoomStrategy.java
@@ -1,13 +1,25 @@
 package com.example.family_artifact_register.UI.MapServiceFragment;
 
 import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 
 import java.util.List;
 
 public class SingleMarkerZoomStrategy extends BaseMakerZoomStrategy {
+    private static final float ZOOM = 8;
+
     @Override
     CameraUpdate makeCameraUpdate(List<Marker> markers) {
-        return null;// TODO (Dovermore, 2019-09-22): Implement makeCameraUpdate
+        assert markers.size() == 1;
+        Marker marker = markers.get(0);
+        CameraPosition cp = CameraPosition
+                .builder()
+                .zoom(ZOOM)
+                .target(marker.getPosition())
+                .build();
+        return CameraUpdateFactory.newCameraPosition(cp);
     }
 }
