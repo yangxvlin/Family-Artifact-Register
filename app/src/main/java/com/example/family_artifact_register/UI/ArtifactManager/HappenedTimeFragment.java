d4914d3e20c1714ea4158ba86ac7e13062a6c723
yangxvlin <1768528843@qq.com>
2019-09-24 14:25:18 +1000

    improve user's set happened time

--- app/src/main/java/com/example/family_artifact_register/UI/ArtifactManager/HappenedTimeFragment.java
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactManager/HappenedTimeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +13,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.family_artifact_register.IFragment;
 import com.example.family_artifact_register.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HappenedTimeFragment extends Fragment implements IFragment {
     /**
@@ -32,6 +38,22 @@ public class HappenedTimeFragment extends Fragment implements IFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle(R.string.artifact_happened_title);
+
+        // get user chosen date
+        DatePicker datePicker = view.findViewById(R.id.fragment_new_artifact_happened_time_date_picker);
+        datePicker.setOnDateChangedListener((datePicker1, year, monthOfYear, dayOfMonth) -> {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(year, monthOfYear, dayOfMonth);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            Toast.makeText(getContext(), format.format(calendar.getTime()), Toast.LENGTH_SHORT)
+                    .show();
+        });
+
+        // to next fragment
+        FloatingActionButton confirm = view.findViewById(R.id.fragment_new_artifact_happened_time_floating_button_confirm);
+        confirm.setOnClickListener(view1 -> {
+
+        });
     }
 
     public static HappenedTimeFragment newInstance() { return new HappenedTimeFragment(); }
