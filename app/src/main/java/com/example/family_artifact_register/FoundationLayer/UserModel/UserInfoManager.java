5e766e249614ea7619437a9a11c3142d5e1bc08d
Dovermore <calvinyeye0627@gmail.com>
2019-09-30 02:47:46 +1000

    remove redundant imports

--- app/src/main/java/com/example/family_artifact_register/FoundationLayer/UserModel/UserInfoManager.java
+++ app/src/main/java/com/example/family_artifact_register/FoundationLayer/UserModel/UserInfoManager.java
@@ -1,25 +1,13 @@
 package com.example.family_artifact_register.FoundationLayer.UserModel;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 import android.util.Pair;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.family_artifact_register.FoundationLayer.DBConstant;
-import com.example.family_artifact_register.MainActivity2;
-import com.example.family_artifact_register.UI.Upload.PostActivity;
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
