d2985fba3d4774692c5790dde227b29ba64139ee
Dovermore <calvinyeye0627@gmail.com>
2019-09-29 23:47:46 +1000

    update a livedata version of userinfomanager

--- app/src/main/java/com/example/family_artifact_register/FoundationLayer/UserModel/UserInfoManager.java
+++ app/src/main/java/com/example/family_artifact_register/FoundationLayer/UserModel/UserInfoManager.java
@@ -1,11 +1,16 @@
 package com.example.family_artifact_register.FoundationLayer.UserModel;
 
 import android.util.Log;
+import android.util.Pair;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.example.family_artifact_register.FoundationLayer.DBConstant;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -25,9 +30,15 @@ public class UserInfoManager {
     }
 
     /**
-     * A static map storing all retrieved user information
+     * A map storing all retrieved user information
      */
-    private static Map<String, UserInfo> userInfoMap = new HashMap<>();
+    private Map<String, MutableLiveData<UserInfo>> userInfoMap = new HashMap<>();
+
+    /**
+     * Active listeners used (this should be cleared if not used)
+     */
+    private Map<String, Pair<ListenerRegistration, Integer>> listenerRegistrationMap;
+
 
     /**
      * The particular user that's using the app
@@ -42,26 +53,81 @@ public class UserInfoManager {
     private UserInfoManager() {
         uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         db = FirebaseFirestore.getInstance();
+        listenerRegistrationMap = new HashMap<>();
     }
 
-    public void getUserInfo(String uid, Callback<UserInfo> callback) {
-        if (userInfoMap.containsKey(uid)) {
-            if (callback != null) callback.callback(userInfoMap.get(uid));
-            return;
+    /**
+     * Get Detailed user information of some users
+     * @param uid one user id
+     * @return A LiveData object containing user information, that will be updated in real time!
+     */
+    public LiveData<UserInfo> getUserInfo(String uid) {
+        // If currently either this uid hasn't been listened yet
+        if (!listenerRegistrationMap.containsKey(uid)) {
+            // Create Entry if not in there
+            if (!userInfoMap.containsKey(uid)) {
+                userInfoMap.put(uid, new MutableLiveData<>());
+            }
+            // Not yet have it in cache
+            ListenerRegistration snapshotListener = db
+                    .collection(DBConstant.USERS)
+                    .document(uid)
+                    .addSnapshotListener((documentSnapshot, e) -> {
+                // Catch and log the error
+                if (e != null) {
+                    Log.w(TAG, "listen:error", e);
+                    return;
+                }
+                // Successfully fetched data
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    userInfoMap.get(uid).setValue(documentSnapshot.toObject(UserInfo.class));
+                } else {
+                    Log.d(TAG,"get failed: user not exists " + uid);
+                }
+            });
+            // Register the listener with the number of users
+            if (!listenerRegistrationMap.containsKey(uid)) {
+                listenerRegistrationMap.put(uid, new Pair<>(snapshotListener, 1));
+            } else {
+                listenerRegistrationMap.put(uid,
+                        new Pair<>(listenerRegistrationMap.get(uid).first,
+                                listenerRegistrationMap.get(uid).second+1));
+            }
+        } else {
+            listenerRegistrationMap.put(uid,
+                    new Pair<>(listenerRegistrationMap.get(uid).first,
+                            listenerRegistrationMap.get(uid).second+1));
         }
+        return userInfoMap.get(uid);
+    }
 
+    /**
+     * Get Detailed user information of some users
+     * @param uids list of user id (duplication will be removed)
+     * @return A List LiveData object containing user information, that will be updated in real time!
+     */
+    public List<LiveData<UserInfo>> getUserInfo(List<String> uids) {
+        ArrayList<LiveData<UserInfo>> liveDataList = new ArrayList<>();
+        for (String uid: new HashSet<>(uids)) {
+            liveDataList.add(getUserInfo(uid));
+        }
+        return liveDataList;
+    }
+
+    /**
+     * Preload the Cache with a list of user id and their info
+     * @param uids The list of uid the user is loading
+     */
+    public void warmCache(ArrayList<String> uids) {
         db.collection("users").document(uid).get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
-                DocumentSnapshot doc = task.getResult();
-                if (doc.exists()) {
-                    UserInfo userInfo;
-                    if (userInfoMap.containsKey(uid)) {
-                        userInfo = userInfoMap.get(uid);
-                    } else {
-                        userInfo = doc.toObject(UserInfo.class);
-                        userInfoMap.put(uid, userInfo);
+                DocumentSnapshot documentSnapshot = task.getResult();
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    // Successfully fetched data
+                    if (!userInfoMap.containsKey(uid)) {
+                        userInfoMap.put(uid, new MutableLiveData<>());
                     }
-                    if (callback!=null) callback.callback(userInfo);
+                    userInfoMap.get(uid).setValue(documentSnapshot.toObject(UserInfo.class));
                 } else {
                     Log.d(TAG,"get failed: user not exists " + uid);
                 }
@@ -72,35 +138,19 @@ public class UserInfoManager {
     }
 
     /**
-     * Get Detailed user information of some users
-     * @param uids list of user id
-     * @param callback
+     * Clean the cache built by this adapter. Also clears ALL listeners
      */
-    public void getUserInfo(List<String> uids,
-                            Callback<Map<String, UserInfo>> callback) {
-        Map<String, UserInfo> res = new HashMap<>();
-        for (String uid: new HashSet<>(uids)) {
-            getUserInfo(uid, info -> {
-                res.put(uid, info);
-                if (res.size()==uids.size()) {
-                    callback.callback(res);
-                }
-            });
+    public void removeListener(String uid) {
+        if (listenerRegistrationMap.containsKey(uid)) {
+            listenerRegistrationMap.put(uid,
+                    new Pair<>(listenerRegistrationMap.get(uid).first,
+                            listenerRegistrationMap.get(uid).second-1));
+            if (listenerRegistrationMap.get(uid).second == 0) {
+                listenerRegistrationMap.remove(uid);
+            }
+        } else {
+            Log.w(TAG, "attempted to remove listener while no listener is attached");
         }
-    }
-
-    /**
-     * Preload the Cache with a list of user id and their info
-     * @param uids The list of uid the user is loading
-     */
-    public void warmCache(ArrayList<String> uids) {
-        for (String uid: uids) {
-            getUserInfo(uid,null);
-        }
-    }
-
-    public interface Callback<T> {
-        void callback(T t);
     }
 
     public void storeUserInfo(UserInfo userInfo) {
