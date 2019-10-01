4e712799145fa43657e15bea97772234c25bef01
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-01 19:53:11 +1000

    add Post Detail View Model

--- app/src/main/java/com/example/family_artifact_register/PresentationLayer/HubPresenter/PostDetailViewModel.java
+++ app/src/main/java/com/example/family_artifact_register/PresentationLayer/HubPresenter/PostDetailViewModel.java
@@ -1,4 +1,4 @@
-package com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter;
+package com.example.family_artifact_register.PresentationLayer.HubPresenter;
 
 import android.app.Application;
 import android.util.Log;
@@ -9,14 +9,14 @@ import androidx.lifecycle.LiveData;
 import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactItem;
 import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactManager;
 
-public class ArtifactDetailViewModel extends AndroidViewModel {
+public class PostDetailViewModel extends AndroidViewModel {
 
-    public static final String TAG = ArtifactDetailViewModel.class.getSimpleName();
+    public static final String TAG = PostDetailViewModel.class.getSimpleName();
 
     private LiveData<ArtifactItem> selectedPost;
     private ArtifactManager manager;
 
-    public ArtifactDetailViewModel(Application application, String selectedPid) {
+    public PostDetailViewModel(Application application, String selectedPid) {
         super(application);
         Log.i(TAG, "enter view model cons");
         manager = ArtifactManager.getInstance();
