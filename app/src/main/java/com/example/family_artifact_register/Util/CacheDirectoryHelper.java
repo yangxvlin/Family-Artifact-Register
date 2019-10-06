24225998a26bca6026bbd5cff2da236731bd38ff
yangxvlin <1768528843@qq.com>
2019-10-06 14:51:00 +1100

    get phone cache directory

--- app/src/main/java/com/example/family_artifact_register/Util/CacheDirectoryHelper.java
+++ app/src/main/java/com/example/family_artifact_register/Util/CacheDirectoryHelper.java
@@ -3,7 +3,22 @@ package com.example.family_artifact_register.Util;
 import java.io.File;
 
 public class CacheDirectoryHelper {
-    private File cacheDirectory;
+    private File cacheDirectory = null;
 
+    private static final CacheDirectoryHelper ourInstance = new CacheDirectoryHelper();
 
+    public static CacheDirectoryHelper getInstance() {
+        return ourInstance;
+    }
+
+    private CacheDirectoryHelper() {
+    }
+
+    public void setCacheDirectory(File cacheDirectory) {
+        this.cacheDirectory = cacheDirectory;
+    }
+
+    public String getCacheDirectory() {
+        return cacheDirectory.getPath();
+    }
 }
