c09c6d22b6fc49f5371baace6531295b936078a3
chen-dudu <liguoc@student.unimelb.edu.au>
2019-10-09 10:02:19 +1100

    finished implemeting timeline, waiting for other parts to do debugging

---
+++ app/src/main/java/com/example/family_artifact_register/PresentationLayer/ArtifactManagerPresenter/TimelineViewModelFactory.java
@@ -0,0 +1,22 @@
+package com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class TimelineViewModelFactory implements ViewModelProvider.Factory {
+
+    private Application application;
+
+    public TimelineViewModelFactory(Application application) {
+        this.application = application;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new TimelineViewModel(application);
+    }
+}
--- app/src/main/java/com/example/family_artifact_register/UI/ArtifactTimeline/TimelineActivity.java
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactTimeline/TimelineActivity.java
@@ -1,6 +1,8 @@
 package com.example.family_artifact_register.UI.ArtifactTimeline;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,55 +11,111 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactItem;
+import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactTimeline;
+import com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.TimelineViewModel;
+import com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.TimelineViewModelFactory;
 import com.example.family_artifact_register.R;
 import com.github.vipulasri.timelineview.TimelineView;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 public class TimelineActivity extends AppCompatActivity {
 
+    public static final String TAG = TimelineActivity.class.getSimpleName();
+
     private RecyclerView recyclerView;
+    private TimelineAdapter adapter;
+
+    private TimelineViewModel viewModel;
+
+    private List<ArtifactItem> artifacts;
+
+    private String timelineID;
+
+    private LifecycleOwner owner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-
-        getSupportActionBar().setTitle("My Artifact timeline");
-
-        // to be replaced with string of artifact items
-        String[] x = new String[] {"1", "2", "3", "4", "5", "6"};
-        ArrayList<String> data = new ArrayList<>();
-        Collections.addAll(data, x);
+        owner = this;
+//        Intent intent = getIntent();
+//        timelineID = intent.getStringExtra("timelineID");
 
         recyclerView = (RecyclerView) findViewById(R.id.timeline_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new Myadapter(data));
+        adapter = new TimelineAdapter();
+        recyclerView.setAdapter(adapter);
 
+        viewModel = ViewModelProviders.of(this, new TimelineViewModelFactory(getApplication())).get(TimelineViewModel.class);
+
+        viewModel.getTimeline(timelineID).observe(owner, new Observer<ArtifactTimeline>() {
+            @Override
+            public void onChanged(ArtifactTimeline artifactTimeline) {
+                Log.d(TAG, "get timeline from DB");
+
+                getSupportActionBar().setTitle(artifactTimeline.getTitle());
+
+                viewModel.getArtifactItems(artifactTimeline.getArtifactItemPostIds()).observe(owner, new Observer<List<ArtifactItem>>() {
+                    @Override
+                    public void onChanged(List<ArtifactItem> newData) {
+                        Log.d(TAG, "get artifacts from DB, using postIDs in timeline");
+                        adapter.setData(newData);
+                    }
+                });
+            }
+        });
+
+        // TODO what happens when back arrow is clicked (who is the parent)
     }
 
-    public class Myadapter extends RecyclerView.Adapter<Myadapter.Myviewholder> {
+    public class TimelineAdapter extends RecyclerView.Adapter<TimelineAdapter.TimelineViewHolder> {
 
-        private ArrayList<String> dataSet;
+        public class TimelineViewHolder extends RecyclerView.ViewHolder {
 
-        public Myadapter(ArrayList<String> dataSet) {
-            this.dataSet = dataSet;
+            public TimelineView timelineView;
+            public TextView time;
+            public TextView title;
+            public TextView description;
+            // to be changed to fragment to handle different types of medias
+            public ImageView media;
+            public String itemId;
+
+            public TimelineViewHolder(@NonNull View itemView, int viewType) {
+                super(itemView);
+                this.timelineView = itemView.findViewById(R.id.timeline);
+                this.time= itemView.findViewById(R.id.timeline_item_upload_time);
+                this.title = itemView.findViewById(R.id.timeline_item_title);
+                this.description = itemView.findViewById(R.id.timeline_item_description);
+                this.media = itemView.findViewById(R.id.timeline_item_media);
+                timelineView.initLine(viewType);
+            }
         }
+
+        private List<ArtifactItem> dataSet;
+
+        public TimelineAdapter() {}
 
         @NonNull
         @Override
-        public Myadapter.Myviewholder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public TimelineAdapter.TimelineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = View.inflate(parent.getContext(), R.layout.timeline_item, null);
-            return new Myviewholder(view, viewType);
+            return new TimelineViewHolder(view, viewType);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull Myviewholder holder, int position) {
-            holder.title.setText(dataSet.get(position));
+        public void onBindViewHolder(@NonNull TimelineViewHolder holder, int position) {
+            holder.time.setText(dataSet.get(position).getUploadDateTime());
+            holder.description.setText(dataSet.get(position).getDescription());
+            holder.itemId = dataSet.get(position).getPostId();
+//            holder.media.setImageResource(dataSet);
         }
 
         @Override
@@ -70,23 +128,9 @@ public class TimelineActivity extends AppCompatActivity {
             return TimelineView.getTimeLineViewType(position, getItemCount());
         }
 
-        public class Myviewholder extends RecyclerView.ViewHolder {
-
-            public TimelineView timelineView;
-            public TextView time;
-            public TextView title;
-            public TextView description;
-            public ImageView photo;
-
-            public Myviewholder(@NonNull View itemView, int viewType) {
-                super(itemView);
-                this.timelineView = itemView.findViewById(R.id.timeline);
-                this.time= itemView.findViewById(R.id.timeline_item_time);
-                this.title = itemView.findViewById(R.id.timeline_item_title);
-                this.description = itemView.findViewById(R.id.timeline_item_description);
-                this.photo = itemView.findViewById(R.id.timeline_item_photo);
-                timelineView.initLine(viewType);
-            }
+        public void setData(List<ArtifactItem> newData) {
+            dataSet = newData;
+            notifyDataSetChanged();
         }
     }
 }
