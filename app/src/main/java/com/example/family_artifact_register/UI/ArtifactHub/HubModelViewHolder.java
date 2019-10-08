f90bd42f848d38bb8f58c0a1551185ecac2df525
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-09 00:49:06 +1100

    enhance hub model view holder

--- app/src/main/java/com/example/family_artifact_register/UI/ArtifactHub/HubModelViewHolder.java
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactHub/HubModelViewHolder.java
@@ -3,35 +3,38 @@ package com.example.family_artifact_register.UI.ArtifactHub;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.family_artifact_register.R;
 
-public class HubModelViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class HubModelViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    public TextView username, title, description;
-    public ImageView avatar, postImage;
+            public TextView username, publisher, description;
+            public ImageView avatar;
 
-    public HubModelViewHolder(View itemView) {
-        super(itemView);
-        itemView.setOnClickListener(this);
-        this.username = itemView.findViewById(R.id.username);
-        this.title = itemView.findViewById(R.id.title);
-        this.description = itemView.findViewById(R.id.description);
-        this.avatar = itemView.findViewById(R.id.avatar);
-        this.postImage = itemView.findViewById(R.id.post_image);
-    }
+            public FrameLayout postImage;
 
-    @Override
-    public void onClick(View view) {
-        String value = (String) username.getText();
-        Context context = view.getContext();
-        Intent i = new Intent(context, ArtifactDetailActivity.class);
-        i.putExtra("key", value);
-        context.startActivity(i);
-    }
+            public String itemId;
 
-}
+            public HubModelViewHolder(View itemView) {
+                super(itemView);
+                itemView.setOnClickListener(this);
+                this.username = itemView.findViewById(R.id.username);
+                this.publisher = itemView.findViewById(R.id.publisher);
+                this.description = itemView.findViewById(R.id.description);
+                this.avatar = itemView.findViewById(R.id.avatar);
+                this.postImage = itemView.findViewById(R.id.post_image);
+            }
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), ArtifactDetailActivity.class);
+                i.putExtra("selectedPid", itemId);
+                startActivity(i);
+            }
+        }
