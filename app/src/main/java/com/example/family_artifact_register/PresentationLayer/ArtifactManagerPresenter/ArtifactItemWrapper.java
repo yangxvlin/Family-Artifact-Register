fb85d05407070eae9662e5b242a1fc66f46250a1
yangxvlin <1768528843@qq.com>
2019-10-10 16:24:59 +1100

    add wrapper

---
+++ app/src/main/java/com/example/family_artifact_register/PresentationLayer/ArtifactManagerPresenter/ArtifactItemWrapper.java
@@ -0,0 +1,7 @@
+package com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter;
+
+/**
+ * a wrapper class that store media's local address
+ */
+public class ArtifactItemWrapper {
+}
