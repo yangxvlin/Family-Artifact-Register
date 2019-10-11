fb6c0736ca579d7cd2188fbf8ef8b42484be0211
yangxvlin <1768528843@qq.com>
2019-10-11 23:58:50 +1100

    add my and friend fragment to be navigated inside the event fragment

--- app/src/main/java/com/example/family_artifact_register/UI/Event/EventFragment.java
+++ app/src/main/java/com/example/family_artifact_register/UI/Event/EventFragment.java
@@ -9,9 +9,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.family_artifact_register.IFragment;
 import com.example.family_artifact_register.R;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class EventFragment extends Fragment implements IFragment {
     /**
@@ -26,13 +30,28 @@ public class EventFragment extends Fragment implements IFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        Log.v(TAG, "me fragment created");
+        Log.v(TAG, "event fragment created");
         return inflater.inflate(R.layout.fragment_event, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getChildFragmentManager(), FragmentPagerItems.with(getContext())
+//                .add(R.string.title, PageFragment.class)
+//                .add(R.string.title, WithArgumentsPageFragment.class, new Bundler().putString("key", "value").get()),
+//                 .add("title", PageFragment.class)
+                .add(getString(R.string.event_friend_title), FriendEventFragment.class)
+                .add(getString(R.string.event_my_title), MyEventFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.fragment_event_view_pager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) view.findViewById(R.id.fragment_event_view_pager_tab);
+        viewPagerTab.setViewPager(viewPager);
     }
 
     /**
