1931e42c0a3dbfad170115bdab7eb2d42d7ffa2e
Dovermore <calvinyeye0627@gmail.com>
2019-10-11 19:57:09 +1100

    remove name field in signup

--- app/src/main/java/com/example/family_artifact_register/FamilyArtifactRegisterActivity.java
+++ app/src/main/java/com/example/family_artifact_register/FamilyArtifactRegisterActivity.java
@@ -44,7 +44,7 @@ public class FamilyArtifactRegisterActivity extends AppCompatActivity implements
      * Available sign-in providers
      */
     List<AuthUI.IdpConfig> providers = Arrays.asList(
-            new AuthUI.IdpConfig.EmailBuilder().build(),
+            new AuthUI.IdpConfig.EmailBuilder().setRequireName(false).build(),
             new AuthUI.IdpConfig.PhoneBuilder().build(),
             new AuthUI.IdpConfig.GoogleBuilder().build(),
             new AuthUI.IdpConfig.FacebookBuilder().build());
