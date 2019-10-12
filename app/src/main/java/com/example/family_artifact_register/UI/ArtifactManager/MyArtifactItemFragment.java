3f886d28395acb2ad6913604ca3d18b7fce1e9a1
yangxvlin <1768528843@qq.com>
2019-10-12 16:50:23 +1100

    add new fragment

---
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactManager/MyArtifactItemFragment.java
@@ -0,0 +1,8 @@
+package com.example.family_artifact_register.UI.ArtifactManager;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.family_artifact_register.IFragment;
+
+public class MyArtifactItemFragment extends Fragment implements IFragment {
+}
