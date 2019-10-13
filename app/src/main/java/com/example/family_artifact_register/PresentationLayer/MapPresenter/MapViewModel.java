85c9ac79087169934bc4a42b6478d28846790ec7
chen-dudu <liguoc@student.unimelb.edu.au>
2019-10-14 09:51:16 +1100

    enable view model to retrieve required data from DB

--- app/src/main/java/com/example/family_artifact_register/PresentationLayer/MapPresenter/MapViewModel.java
+++ app/src/main/java/com/example/family_artifact_register/PresentationLayer/MapPresenter/MapViewModel.java
@@ -7,14 +7,17 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
 import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactItem;
 import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactManager;
+import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactTimeline;
 import com.example.family_artifact_register.FoundationLayer.MapModel.MapLocation;
 import com.example.family_artifact_register.FoundationLayer.MapModel.MapLocationManager;
 import com.example.family_artifact_register.FoundationLayer.UserModel.UserInfo;
 import com.example.family_artifact_register.FoundationLayer.UserModel.UserInfoManager;
+import com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.ArtifactItemWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,6 +35,8 @@ public class MapViewModel extends AndroidViewModel {
     private List<MapLocation> mapLocations = new ArrayList<>();
 
     private MediatorLiveData<List<MapLocation>> locations = new MediatorLiveData<>();
+
+    private MutableLiveData<List<TimelineMapWrapper>> timelineWrappers = new MutableLiveData<>();
 
     public MapViewModel(@NonNull Application application) {
         super(application);
@@ -59,5 +64,50 @@ public class MapViewModel extends AndroidViewModel {
             }
         });
         return locations;
+    }
+
+    public LiveData<List<TimelineMapWrapper>> getMapWrapper() {
+
+        // TODO choose a good listener identifier
+        artifactManager.listenArtifactTimelineByUid(userInfoManager.getCurrentUid(), ).observeForever(new Observer<List<ArtifactTimeline>>() {
+            @Override
+            public void onChanged(List<ArtifactTimeline> artifactTimelines) {
+                Log.d(TAG, "retrieved all timeline data for current user");
+                List<TimelineMapWrapper> wrappers = new ArrayList<>();
+                timelineWrappers.postValue(wrappers);
+
+                for(ArtifactTimeline timeline: artifactTimelines) {
+                    List<ArtifactItemWrapper> itemWrappers = new ArrayList<>();
+                    List<MapLocation> mapLocations = new ArrayList<>();
+                    TimelineMapWrapper timelineWrapper = new TimelineMapWrapper(timeline, itemWrappers, mapLocations);
+                    wrappers.add(timelineWrapper);
+                    timelineWrappers.postValue(wrappers);
+
+                    List<String> itemIDs = timeline.getArtifactItemPostIds();
+                    for(String id: itemIDs) {
+
+                        // TODO choose a good listener identifier
+                        artifactManager.listenArtifactItemByPostId(id, ).observeForever(new Observer<ArtifactItem>() {
+                            @Override
+                            public void onChanged(ArtifactItem artifactItem) {
+                                Log.d(TAG, "retrieved data about artifact item with id: " + artifactItem.getPostId());
+
+                                timelineWrapper.getArtifactItemWrapperList().add(new ArtifactItemWrapper(artifactItem));
+                                timelineWrappers.postValue(wrappers);
+                                mapLocationManager.getMapLocationById(artifactItem.getLocationStoredId()).observeForever(new Observer<MapLocation>() {
+                                    @Override
+                                    public void onChanged(MapLocation mapLocation) {
+                                        timelineWrapper.getStoreLocationList().add(mapLocation);
+                                        timelineWrappers.postValue(wrappers);
+                                    }
+                                });
+                            }
+                        });
+
+                    }
+                }
+            }
+        });
+        return timelineWrappers;
     }
 }
