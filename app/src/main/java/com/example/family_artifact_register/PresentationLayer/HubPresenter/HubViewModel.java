3a87ede79137dbb8d96f6090130eec6ed3e3437e
chen-dudu <liguoc@student.unimelb.edu.au>
2019-10-14 09:08:17 +1100

    enable view model to retrieve artifact item data of all friends

--- app/src/main/java/com/example/family_artifact_register/PresentationLayer/HubPresenter/HubViewModel.java
+++ app/src/main/java/com/example/family_artifact_register/PresentationLayer/HubPresenter/HubViewModel.java
@@ -18,6 +18,7 @@ import com.example.family_artifact_register.FoundationLayer.UserModel.UserInfo;
 import com.example.family_artifact_register.FoundationLayer.UserModel.UserInfoManager;
 import com.example.family_artifact_register.FoundationLayer.Util.FirebaseStorageHelper;
 import com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.ArtifactItemWrapper;
+import com.example.family_artifact_register.PresentationLayer.SocialPresenter.UserInfoWrapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,55 +30,132 @@ public class HubViewModel extends AndroidViewModel {
     public static final String TAG = HubViewModel.class.getSimpleName();
 
     private UserInfoManager userInfoManager = UserInfoManager.getInstance();
-
     private ArtifactManager artifactManager = ArtifactManager.getInstance();
-
     private FirebaseStorageHelper fSHelper = FirebaseStorageHelper.getInstance();
 
     private MutableLiveData<List<ArtifactItem>> artifactList;
-
     private MutableLiveData<List<ArtifactItemWrapper>> artifactWrapperList;
 
+    private List<ArtifactItem> latestArtifactList = new ArrayList<>();
+    private MutableLiveData<List<ArtifactItemWrapper>> latestArtifactWrapperList = new MutableLiveData<>();
+
+    private List<String> latestPostList = new ArrayList<>();
+
     private String currentUid;
+    private List<String> friendUids;
+
+    private MediatorLiveData<List<UserInfoWrapper>> friends = new MediatorLiveData<>();
 
     public HubViewModel(Application application) {
         super(application);
-        getPostsChange();
-    }
 
-    public void getPostsChange() {
         currentUid = userInfoManager.getCurrentUid();
-        artifactList = (MutableLiveData<List<ArtifactItem>>) artifactManager.getArtifactItemByUid(currentUid);
-        artifactWrapperList = new MutableLiveData<>();
-        ArrayList<ArtifactItemWrapper> wrappers = new ArrayList<>();
-        artifactWrapperList.setValue(wrappers);
-        artifactList.observeForever(new Observer<List<ArtifactItem>>() {
+
+        userInfoManager.listenUserInfo(currentUid).observeForever(new Observer<UserInfo>() {
             @Override
-            public void onChanged(List<ArtifactItem> artifactItems) {
-                for(ArtifactItem item: artifactItems) {
-                    List<String> mediaDataRemoteUrls = item.getMediaDataUrls();
-                    ArtifactItemWrapper wrapper = new ArtifactItemWrapper(item);
+            public void onChanged(UserInfo me) {
+                Log.d(TAG, "retrieved latest data about current user");
+                friendUids = new ArrayList<>(me.getFriendUids().keySet());
+                List<LiveData<UserInfo>> friendList = userInfoManager.listenUserInfo(friendUids);
 
-                    fSHelper.loadByRemoteUri(mediaDataRemoteUrls).observeForever(new Observer<List<Uri>>() {
+                friends.setValue(new ArrayList<>());
+
+                for(LiveData<UserInfo> friend: friendList) {
+                    friends.removeSource(friend);
+                    friends.addSource(friend, new Observer<UserInfo>() {
                         @Override
-                        public void onChanged(List<Uri> uris) {
-                            Log.d(TAG, "local uris: " + uris.toString());
+                        public void onChanged(UserInfo userInfo) {
+                            Log.d(TAG, "retrieved data about user with uid: " + userInfo.getUid());
 
-                            // load data to wrapper
-                            wrapper.setLocalMediaDataUrls(
-                                    uris.stream()
-                                            .map(Objects::toString)
-                                            .collect(Collectors.toList())
-                            );
+                            // processing this user's info
+                            UserInfoWrapper wrapper = new UserInfoWrapper(userInfo);
+                            friends.getValue().add(wrapper);
+                            friends.setValue(friends.getValue());
+                            String url = wrapper.getPhotoUrl();
+                            if(url == null) {
+                                wrapper.setPhotoUrl(null);
+                                friends.setValue(friends.getValue());
+                            }
+                            else {
+                                fSHelper.loadByRemoteUri(url).observeForever(new Observer<Uri>() {
+                                    @Override
+                                    public void onChanged(Uri uri) {
+                                        wrapper.setPhotoUrl(uri.toString());
+                                        friends.setValue(friends.getValue());
+                                    }
+                                });
+                            }
 
-                            wrappers.add(wrapper);
-                            artifactWrapperList.setValue(wrappers);
-                            // artifactList.setValue(artifactList.getValue());
+                            // now processing this user's artifact info
+                            // TODO set an appropriate listener identifier
+                            artifactManager.listenArtifactItemByUid(userInfo.getUid(), ).observeForever(new Observer<List<ArtifactItem>>() {
+                                @Override
+                                public void onChanged(List<ArtifactItem> artifactItems) {
+                                    Log.d(TAG, "retrieved artifact item data about user with uid: " + userInfo.getUid());
+                                    ArrayList<ArtifactItemWrapper> wrappers = new ArrayList<>();
+                                    latestArtifactWrapperList.postValue(wrappers);
+                                    for(ArtifactItem artifactItem: artifactItems) {
+                                        ArtifactItemWrapper wrapper = new ArtifactItemWrapper(artifactItem);
+                                        wrappers.add(wrapper);
+                                        latestArtifactWrapperList.postValue(wrappers);
+                                        List<String> urls = artifactItem.getMediaDataUrls();
+                                        if(urls.size() > 0) {
+                                            fSHelper.loadByRemoteUri(urls).observeForever(new Observer<List<Uri>>() {
+                                                @Override
+                                                public void onChanged(List<Uri> uris) {
+                                                    wrapper.setLocalMediaDataUrls(
+                                                            uris.stream()
+                                                                    .map(Objects::toString)
+                                                                    .collect(Collectors.toList()));
+                                                    latestArtifactWrapperList.postValue(wrappers);
+                                                }
+                                            });
+                                        }
+                                    }
+                                }
+                            });
                         }
                     });
                 }
             }
         });
+        getPostsChange();
+    }
+
+    public void getPostsChange() {
+        artifactWrapperList.postValue(latestArtifactWrapperList.getValue());
+//        currentUid = userInfoManager.getCurrentUid();
+//        artifactList = (MutableLiveData<List<ArtifactItem>>) artifactManager.getArtifactItemByUid(currentUid);
+//        artifactWrapperList = new MutableLiveData<>();
+//        ArrayList<ArtifactItemWrapper> wrappers = new ArrayList<>();
+//        artifactWrapperList.setValue(wrappers);
+//        artifactList.observeForever(new Observer<List<ArtifactItem>>() {
+//            @Override
+//            public void onChanged(List<ArtifactItem> artifactItems) {
+//                for(ArtifactItem item: artifactItems) {
+//                    List<String> mediaDataRemoteUrls = item.getMediaDataUrls();
+//                    ArtifactItemWrapper wrapper = new ArtifactItemWrapper(item);
+//
+//                    fSHelper.loadByRemoteUri(mediaDataRemoteUrls).observeForever(new Observer<List<Uri>>() {
+//                        @Override
+//                        public void onChanged(List<Uri> uris) {
+//                            Log.d(TAG, "local uris: " + uris.toString());
+//
+//                            // load data to wrapper
+//                            wrapper.setLocalMediaDataUrls(
+//                                    uris.stream()
+//                                            .map(Objects::toString)
+//                                            .collect(Collectors.toList())
+//                            );
+//
+//                            wrappers.add(wrapper);
+//                            artifactWrapperList.setValue(wrappers);
+//                            // artifactList.setValue(artifactList.getValue());
+//                        }
+//                    });
+//                }
+//            }
+//        });
     }
 
     public LiveData<List<ArtifactItemWrapper>> getPosts() {
