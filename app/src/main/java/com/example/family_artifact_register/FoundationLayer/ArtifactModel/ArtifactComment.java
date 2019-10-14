c52132bb4ade8299257de01c6b74dade631e99f5
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-14 14:41:08 +1100

    finish artifact comment model

--- app/src/main/java/com/example/family_artifact_register/FoundationLayer/ArtifactModel/ArtifactComment.java
+++ app/src/main/java/com/example/family_artifact_register/FoundationLayer/ArtifactModel/ArtifactComment.java
@@ -2,9 +2,65 @@ package com.example.family_artifact_register.FoundationLayer.ArtifactModel;
 
 public class ArtifactComment extends Artifact {
 
-    /**
-     * family artifacts have the comment
-     */
-    private String artifactItemPostId;
 
+    /**
+     * the text of the comment
+     */
+    private String comment;
+
+    /**
+     * the user send comment
+     */
+    private String sender;
+
+
+    /**
+     * Create new artifact comment. This constructor is used by firestore and shouldn't be accessed
+     * externally
+     * @deprecated Use manager/newInstance method to create new artifact. Don't use constructor
+     */
+    public ArtifactComment(String postId, String uid, String uploadDateTime,
+                            String lastUpdateDateTime, String comment, String sender) {
+        super(postId, uid, uploadDateTime, lastUpdateDateTime);
+        this.comment = comment;
+        this.sender = sender;
+    }
+
+    public ArtifactComment() { super(); }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public String getPostId() {
+        return super.getPostId();
+    }
+
+    @Override
+    public String getUid() {
+        return super.getUid();
+    }
+
+    @Override
+    public String getUploadDateTime() {
+        return super.getUploadDateTime();
+    }
+
+    @Override
+    public String getLastUpdateDateTime() {
+        return super.getLastUpdateDateTime();
+    }
 }
