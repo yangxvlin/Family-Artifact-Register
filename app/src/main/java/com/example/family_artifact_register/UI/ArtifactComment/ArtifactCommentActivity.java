2175a90b9b22b47db04a200ceed2fa6d42c523af
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-14 16:10:59 +1100

    add ui comment activity

--- app/src/main/java/com/example/family_artifact_register/UI/ArtifactComment/ArtifactCommentActivity.java
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactComment/ArtifactCommentActivity.java
@@ -1,16 +1,41 @@
 package com.example.family_artifact_register.UI.ArtifactComment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactItem;
+import com.example.family_artifact_register.PresentationLayer.ArtifactCommentPresenter.CommentViewModel;
 import com.example.family_artifact_register.R;
+import com.example.family_artifact_register.UI.ArtifactDetail.ArtifactDetailActivity;
 
 public class ArtifactCommentActivity extends AppCompatActivity {
+
+    public static final String TAG = ArtifactDetailActivity.class.getSimpleName();
+
+    private RecyclerView recyclerView;
+
+    private CommentAdapter commentAdapter;
+
+    private CommentViewModel viewModel;
+
+    private ArtifactItem artifactItem;
+
+    private String PostID;
+
+    private LifecycleOwner owner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artifact_comment);
+        owner = this;
+        Intent intent = getIntent();
+        PostID = intent.getStringExtra("artifactItemPostId");
+
+        commentAdapter = new CommentAdapter(this);
     }
 }
