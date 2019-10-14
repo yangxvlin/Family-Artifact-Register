06126f94e8b57e1e226f1056a7a0f3375bcf397c
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-14 12:51:00 +1100

    show paly icon in detail activity

--- app/src/main/java/com/example/family_artifact_register/UI/ArtifactDetail/ArtifactDetailActivity.java
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactDetail/ArtifactDetailActivity.java
@@ -154,12 +154,11 @@ public class ArtifactDetailActivity extends AppCompatActivity {
                         postImage.addView(mediaView);
                     } else if (artifactItemWrapper.getMediaType() == TYPE_VIDEO) {
                         // video view
+                        ImageView playIcon = getVideoPlayIcon(artifactDetailActivity);
                         mediaView = getVideoThumbnail(750, 750,
                                 mediaList.get(0), artifactDetailActivity);
-
-                        ImageView playIcon = getVideoPlayIcon(artifactDetailActivity);
-                        postImage.addView(playIcon);
                         postImage.addView(mediaView);
+                        postImage.addView(playIcon);
                     } else {
                         Log.e(TAG, "unknown media type !!!");
                     }
