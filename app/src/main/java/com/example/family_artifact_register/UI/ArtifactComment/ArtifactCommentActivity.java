bc67025db33f09cc2e97c1b0e3a9fe3d39230f1b
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-14 14:56:56 +1100

    add comment activity and layout

---
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactComment/ArtifactCommentActivity.java
@@ -0,0 +1,16 @@
+package com.example.family_artifact_register.UI.ArtifactComment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.family_artifact_register.R;
+
+public class ArtifactCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artifact_comment);
+    }
+}
