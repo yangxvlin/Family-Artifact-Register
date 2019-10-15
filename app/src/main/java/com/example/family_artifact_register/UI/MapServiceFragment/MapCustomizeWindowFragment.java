e660f9e603ef42ec6d1c35e72f6dc9fe5ccc3c1f
yangxvlin <1768528843@qq.com>
2019-10-16 01:48:34 +1100

    interactive map

--- app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/MapCustomizeWindowFragment.java
+++ app/src/main/java/com/example/family_artifact_register/UI/MapServiceFragment/MapCustomizeWindowFragment.java
@@ -1,45 +1,54 @@
 package com.example.family_artifact_register.UI.MapServiceFragment;
 
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
+import com.appolica.interactiveinfowindow.InfoWindow;
 import com.appolica.interactiveinfowindow.InfoWindowManager;
+import com.appolica.interactiveinfowindow.customview.TouchInterceptFrameLayout;
 import com.example.family_artifact_register.FoundationLayer.MapModel.MapLocation;
 import com.example.family_artifact_register.IFragment;
 import com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.ArtifactItemWrapper;
 import com.example.family_artifact_register.PresentationLayer.Util.Pair;
 import com.example.family_artifact_register.R;
 import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.family_artifact_register.UI.Util.MediaProcessHelper.TYPE_IMAGE;
-import static com.example.family_artifact_register.UI.Util.MediaProcessHelper.TYPE_VIDEO;
-import static com.example.family_artifact_register.UI.Util.MediaProcessHelper.cropCenter;
-import static com.example.family_artifact_register.UI.Util.MediaProcessHelper.getResizedBitmap;
-import static com.example.family_artifact_register.UI.Util.MediaViewHelper.getVideoThumbNail;
+import static com.example.family_artifact_register.UI.MapServiceFragment.MarkerHelper.getCreateAt;
+import static com.example.family_artifact_register.UI.MapServiceFragment.MarkerHelper.getSnippet;
+import static com.example.family_artifact_register.UI.MapServiceFragment.MarkerHelper.setUpMarker;
 
-public class MapCustomizeWindowFragment extends MapDisplayFragment implements IFragment {
+public class MapCustomizeWindowFragment extends Fragment implements
+        IFragment,
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener {
     /**
      * class tag
      */
     public static final String TAG = MapCustomizeWindowFragment.class.getSimpleName();
 
     private InfoWindowManager infoWindowManager;
+
+    private InfoWindow infoWindow;
+
+    GoogleMap mMap;
+
+    private MapView mapView;
+
+    public static final int IMAGE_DEFAULT_WIDTH = 200;
+    public static final int IMAGE_DEFAULT_HEIGHT = 200;
 
     /**
      * Required empty public constructor
@@ -57,53 +66,81 @@ public class MapCustomizeWindowFragment extends MapDisplayFragment implements IF
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        mapView = view.findViewById(R.id.fragment_map_customize_window_map_view);
+        mapView.onCreate(savedInstanceState);
+        final TouchInterceptFrameLayout mapViewContainer =
+                view.findViewById(R.id.fragment_map_customize_window_map_view_container);
 
+        mapView.getMapAsync(this);
+
+        infoWindowManager = new InfoWindowManager(getFragmentManager());
+        infoWindowManager.onParentViewCreated(mapViewContainer, savedInstanceState);
     }
 
     @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+        infoWindowManager.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        infoWindowManager.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        mMap = googleMap;
+        infoWindowManager.onMapReady(googleMap);
+        googleMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        final InfoWindow.MarkerSpecification markerSpec =
+                new InfoWindow.MarkerSpecification(10, 10);
+
+        infoWindow = new InfoWindow(marker, markerSpec, MyInfoWindow.newInstance(marker));
+
+        if (infoWindow != null) {
+            infoWindowManager.toggle(infoWindow, true);
+        }
+
+        return true;
+    }
+
     public void setDisplayArtifactItems(List<Pair<ArtifactItemWrapper, MapLocation>> artifactItems) {
         // Only display with marker if map is not null and there are locations stored
         if (mMap != null && artifactItems.size() != 0) {
             mMap.clear();
             List<Marker> markers = new ArrayList<>();
             for (Pair<ArtifactItemWrapper, MapLocation> pair : artifactItems) {
-                ArtifactItemWrapper artifactItemWrapper = pair.getFst();
-                MapLocation storeLocation = pair.getSnd();
-
-                // Log.d(getFragmentTag(), "store location = " + storeLocation.toString());
-
-                MarkerOptions opt = new MarkerOptions()
-                        .position(new LatLng(storeLocation.getLatitude(),
-                                storeLocation.getLongitude()))
-                        .title(getContext().getString(R.string.create_at) + artifactItemWrapper.getUploadDateTime())
-                        .snippet(getSnippet(pair));
-
-                if (artifactItemWrapper.getMediaType() == TYPE_IMAGE) {
-                    try {
-                        Uri uri = Uri.parse(artifactItemWrapper.getLocalMediaDataUrls().get(0));
-
-                        if (uri.getScheme() == null) {
-                            uri = Uri.parse("file://" + uri.toString());
-                            Log.d(getFragmentTag(), "uri = " + uri);
-                        }
-
-                        Bitmap mBitmap = MediaStore.Images.Media.getBitmap(
-                                getContext().getContentResolver(),
-                                uri
-                        );
-                        mBitmap = getResizedBitmap(cropCenter(mBitmap), IMAGE_DEFAULT_WIDTH, IMAGE_DEFAULT_HEIGHT);
-                        opt.icon(BitmapDescriptorFactory.fromBitmap(mBitmap));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                } else if (artifactItemWrapper.getMediaType() == TYPE_VIDEO) {
-                    Bitmap mBitmap = getVideoThumbNail(artifactItemWrapper.getLocalMediaDataUrls()
-                            .get(0));
-                    mBitmap = getResizedBitmap(cropCenter(mBitmap), IMAGE_DEFAULT_WIDTH, IMAGE_DEFAULT_HEIGHT);
-                    opt.icon(BitmapDescriptorFactory.fromBitmap(mBitmap));
-                } else {
-                    Log.e(getFragmentTag(), "unknown media Type !!!");
-                }
+                MarkerOptions opt = setUpMarker(pair,
+                        getContext(),
+                        IMAGE_DEFAULT_WIDTH, IMAGE_DEFAULT_HEIGHT,
+                        getCreateAt(pair, getContext()),
+                        getSnippet(pair, getContext()));
 
                 markers.add(mMap.addMarker(opt));
             }
