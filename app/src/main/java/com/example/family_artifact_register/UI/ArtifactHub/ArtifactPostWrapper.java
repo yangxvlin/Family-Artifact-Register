603ac38554b748f1fa426c80e186eb01f1dabd24
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-16 13:24:08 +1100

    add artifact post wrapper

---
+++ app/src/main/java/com/example/family_artifact_register/UI/ArtifactHub/ArtifactPostWrapper.java
@@ -0,0 +1,18 @@
+package com.example.family_artifact_register.UI.ArtifactHub;
+
+import com.example.family_artifact_register.FoundationLayer.ArtifactModel.ArtifactComment;
+import com.example.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.ArtifactItemWrapper;
+import com.example.family_artifact_register.PresentationLayer.SocialPresenter.UserInfoWrapper;
+
+public class ArtifactPostWrapper {
+
+    private ArtifactItemWrapper artifactItemWrapper;
+
+    private UserInfoWrapper userInfoWrapper;
+
+    public ArtifactPostWrapper(ArtifactItemWrapper artifactItemWrapper, UserInfoWrapper userInfoWrapper) {
+        this.artifactItemWrapper = artifactItemWrapper;
+        this.userInfoWrapper = userInfoWrapper;
+    }
+
+}
