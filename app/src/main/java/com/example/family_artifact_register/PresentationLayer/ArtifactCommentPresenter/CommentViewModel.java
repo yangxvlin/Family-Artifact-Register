a0f71c1b9023c9daa71549c4d95c3c86160a4ad3
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-16 21:44:20 +1100

    comment shown

--- app/src/main/java/com/example/family_artifact_register/PresentationLayer/ArtifactCommentPresenter/CommentViewModel.java
+++ app/src/main/java/com/example/family_artifact_register/PresentationLayer/ArtifactCommentPresenter/CommentViewModel.java
@@ -42,75 +42,37 @@ public class CommentViewModel extends AndroidViewModel {
     private FirebaseStorageHelper fSHelper = FirebaseStorageHelper.getInstance();
 
     private String PostID;
-    private String currentUid;
-    private List<String> friendUids;
     private LiveData<ArtifactItem> artifact;
-    private MediatorLiveData<List<UserInfoWrapper>> friends = new MediatorLiveData<>();
     private List<CommentWrapper> commentList = new ArrayList<>();
     private MutableLiveData<List<CommentWrapper>> comments = new MutableLiveData<>();
 
     public CommentViewModel(@NonNull Application application, String PostId) {
         super(application);
-        currentUid = userInfoManager.getCurrentUid();
         this.PostID = PostId;
         comments.postValue(commentList);
-
-        userInfoManager.listenUserInfo(currentUid).observeForever(new Observer<UserInfo>() {
-            @Override
-            public void onChanged(UserInfo me) {
-                Log.d(TAG, "retrieved latest data about current user");
-                friendUids = new ArrayList<>(me.getFriendUids().keySet());
-                ArrayList<CommentWrapper> commentWrappers = new ArrayList<>();
-                List<LiveData<UserInfo>> friendList = userInfoManager.listenUserInfo(friendUids);
-
-                friends.setValue(new ArrayList<>());
-                Log.d(TAG, "size of friend list: " + friendList.size());
-                for(LiveData<UserInfo> friend: friendList) {
-                    friends.removeSource(friend);
-                    friends.addSource(friend, new Observer<UserInfo>() {
-                        @Override
-                        public void onChanged(UserInfo userInfo) {
-                            Log.d(TAG, "retrieved data about user with uid: " + userInfo.getUid());
-
-                            // processing this user's info
-                            UserInfoWrapper userWrapper = new UserInfoWrapper(userInfo);
-//                            friends.getValue().add(wrapper);
-//                            friends.setValue(friends.getValue());
-                            String url = userWrapper.getPhotoUrl();
-                            if(url == null) {
-                                userWrapper.setPhotoUrl(null);
-                                friends.getValue().add(userWrapper);
-                                friends.setValue(friends.getValue());
-                                getComment(userWrapper, commentWrappers);
-                            }
-                            else {
-                                fSHelper.loadByRemoteUri(url).observeForever(new Observer<Uri>() {
-                                    @Override
-                                    public void onChanged(Uri uri) {
-                                        userWrapper.setPhotoUrl(uri.toString());
-//                                        friends.setValue(friends.getValue());
-                                        getComment(userWrapper, commentWrappers);
-                                    }
-                                });
-                            }
-                        }
-                    });
-                }
-            }
-        });
+        getComment();
     }
 
-    public void getComment(UserInfoWrapper userInfo, List<CommentWrapper> wrappers) {
+    public void getComment() {
         // now processing this user's artifact info
         artifactManager.listenCommentByArtifact(PostID, "CommentViewModel1").observeForever(new Observer<List<ArtifactComment>>() {
             @Override
             public void onChanged(List<ArtifactComment> artifactComments) {
-                Log.d(TAG, "retrieved comment data about user with uid: " + userInfo.getUid());
+                List<CommentWrapper> wrappers = new ArrayList<>();
                 comments.setValue(wrappers);
                 for(ArtifactComment artifactComment: artifactComments) {
-                    CommentWrapper wrapper = new CommentWrapper(artifactComment, userInfo);
-                    wrappers.add(wrapper);
-                    comments.postValue(wrappers);
+                    Log.d(TAG, "retrieved comment data about user with comment: " + artifactComment.getContent());
+                    userInfoManager.listenUserInfo(artifactComment.getUid()).observeForever(new Observer<UserInfo>() {
+                        @Override
+                        public void onChanged(UserInfo userInfo) {
+                            Log.d(TAG, "retrieved comment data about user with uid " + userInfo.getUid());
+                            UserInfoWrapper senderInfo = new UserInfoWrapper(userInfo);
+                            CommentWrapper wrapper = new CommentWrapper(artifactComment, senderInfo);
+                            wrappers.add(wrapper);
+                            comments.postValue(wrappers);
+                        }
+                    });
+
                 }
             }
         });
