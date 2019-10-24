467b908ee63ab421ce6b9461078affeaf8c22bf5
yangxvlin <1768528843@qq.com>
2019-10-24 14:57:41 +1100

    add documentation

--- app/src/main/java/com/unimelb/family_artifact_register/UI/Util/MediaConstant.java
+++ app/src/main/java/com/unimelb/family_artifact_register/UI/Util/MediaConstant.java
@@ -1,8 +1,20 @@
 package com.unimelb.family_artifact_register.UI.Util;
 
+/**
+ * constant for process media
+ * Deprecated because no longer use media fragment
+ * not deleted by the open-close principle
+ */
+@Deprecated
 public class MediaConstant {
+    /**
+     * media type
+     */
     public static String MEDIA_TYPE = "media_type";
 
+    /**
+     * a list of media
+     */
     public static String MEDIA_URL_LIST = "media_list";
 
 }
