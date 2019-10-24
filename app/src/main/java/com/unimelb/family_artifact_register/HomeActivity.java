0fc07c012b88ab099fafef36f905f2f74553e759
yangxvlin <1768528843@qq.com>
2019-10-24 12:10:03 +1100

    add documentation

--- app/src/main/java/com/unimelb/family_artifact_register/HomeActivity.java
+++ app/src/main/java/com/unimelb/family_artifact_register/HomeActivity.java
@@ -24,7 +24,7 @@ import com.unimelb.family_artifact_register.Util.IFragment;
 /**
  * @author XuLin Yang 904904,
  * @time 2019-9-21 13:33:24
- * @description main activity let user to use the app with various bottom navigation
+ * @description main activity let the user use the app with various bottom navigation
  */
 public class HomeActivity extends AppCompatActivity {
     /**
