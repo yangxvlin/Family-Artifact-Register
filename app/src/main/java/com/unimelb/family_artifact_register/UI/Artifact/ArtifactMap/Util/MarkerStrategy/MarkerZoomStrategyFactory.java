3aea5ef0c592094a056aec012f4f2e1d2fe87113
yangxvlin <1768528843@qq.com>
2019-10-25 15:05:02 +1100

    add documentation

--- app/src/main/java/com/unimelb/family_artifact_register/UI/Artifact/ArtifactMap/Util/MarkerStrategy/MarkerZoomStrategyFactory.java
+++ app/src/main/java/com/unimelb/family_artifact_register/UI/Artifact/ArtifactMap/Util/MarkerStrategy/MarkerZoomStrategyFactory.java
@@ -1,11 +1,25 @@
 package com.unimelb.family_artifact_register.UI.Artifact.ArtifactMap.Util.MarkerStrategy;
 
+/**
+ * singleton factory pattern to provide a global access
+ */
 public class MarkerZoomStrategyFactory {
+    /**
+     * singleton instance
+     */
     private static MarkerZoomStrategyFactory markerZoomStrategyFactory = new MarkerZoomStrategyFactory();
+
+    /**
+     * @return factory
+     */
     public static MarkerZoomStrategyFactory getMarkerZoomStrategyFactory() {
         return markerZoomStrategyFactory;
     }
 
+    /**
+     * @param markerLength number of markers
+     * @return strategy to zoom for markers
+     */
     public MarkerZoomStrategy getMarkerZoomStrategy(int markerLength) {
         if (markerLength == 1) {
             return new SingleMarkerZoomStrategy();
