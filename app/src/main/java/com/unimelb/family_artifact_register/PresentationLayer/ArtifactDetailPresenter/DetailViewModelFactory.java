94439ed8938dceda4dc739476fb0790df4d9a9c9
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-26 18:54:01 +1100

    add detail view model factory document

--- app/src/main/java/com/unimelb/family_artifact_register/PresentationLayer/ArtifactDetailPresenter/DetailViewModelFactory.java
+++ app/src/main/java/com/unimelb/family_artifact_register/PresentationLayer/ArtifactDetailPresenter/DetailViewModelFactory.java
@@ -6,6 +6,11 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+/**
+ * @author Haichao Song 854035,
+ * @time 2019-10-06 22:58:09
+ * @description view model factory to create detail view model in activity
+ */
 public class DetailViewModelFactory implements ViewModelProvider.Factory {
 
     private Application application;
