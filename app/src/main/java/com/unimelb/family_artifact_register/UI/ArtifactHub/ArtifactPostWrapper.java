4b795c633b8c7ef2bd31c3cf35947bdab2b3af8b
Haichao Song <haichaos@student.unimelb.edu.au>
2019-10-26 17:43:01 +1100

    add post wrapper document

--- app/src/main/java/com/unimelb/family_artifact_register/UI/ArtifactHub/ArtifactPostWrapper.java
+++ app/src/main/java/com/unimelb/family_artifact_register/UI/ArtifactHub/ArtifactPostWrapper.java
@@ -2,15 +2,27 @@ package com.unimelb.family_artifact_register.UI.ArtifactHub;
 
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
 import com.unimelb.family_artifact_register.PresentationLayer.ArtifactManagerPresenter.ArtifactItemWrapper;
 import com.unimelb.family_artifact_register.PresentationLayer.SocialPresenter.UserInfoWrapper;
 
+/**
+ * @author Haichao Song 854035,
+ * @time 2019-10-13 15:03:48
+ * @description wrapper to pair artifact item with user info in order to show poster avatar and
+ * display name in the hub fragment.
+ */
 public class ArtifactPostWrapper {
 
     private ArtifactItemWrapper artifactItemWrapper;
 
     private UserInfoWrapper userInfoWrapper;
 
+    /**
+     * Construct wrapper
+     * @param artifactItemWrapper the artifact information for post item
+     * @param userInfoWrapper the poster information for post item
+     */
     public ArtifactPostWrapper(ArtifactItemWrapper artifactItemWrapper, UserInfoWrapper userInfoWrapper) {
         this.artifactItemWrapper = artifactItemWrapper;
         this.userInfoWrapper = userInfoWrapper;
