a549e534152a52e33ec8cce99b22249905ad2386
chen-dudu <liguoc@student.unimelb.edu.au>
2019-10-26 16:11:37 +1100

    added comment

--- app/src/main/java/com/unimelb/family_artifact_register/PresentationLayer/SocialPresenter/Request.java
+++ app/src/main/java/com/unimelb/family_artifact_register/PresentationLayer/SocialPresenter/Request.java
@@ -2,20 +2,39 @@ package com.unimelb.family_artifact_register.PresentationLayer.SocialPresenter;
 
 import com.unimelb.family_artifact_register.PresentationLayer.Util.UserInfoWrapper;
 
+/**
+ * a class that represents the friend request in the system
+ */
 public class Request {
 
+    // request sending user
     private UserInfoWrapper user;
+
+    // request sending time
     private String time;
 
+    /**
+     * public constructor for instantiating a new {@link Request}
+     * @param user the sending user
+     * @param time request sending time
+     */
     public Request(UserInfoWrapper user, String time) {
         this.user = user;
         this.time = time;
     }
 
+    /**
+     * get the request sending time
+     * @return the request sending time
+     */
     public String getTime() {
         return time;
     }
 
+    /**
+     * the sending user
+     * @return sending user
+     */
     public UserInfoWrapper getUser() {
         return user;
     }
